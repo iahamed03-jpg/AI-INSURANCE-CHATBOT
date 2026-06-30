a24f9b50fda68f553c92e10767f96cf5b7900e34
iahamed03-jpg <iahamed03@gmail.com>
2026-06-30 19:48:21 +0530

    Modified Application to accomodate both Rest Template and Spring AI

--- src/main/java/com/insurance/ai/chatbot/aiinsurnacechatbot/config/AppConfig.java
+++ src/main/java/com/insurance/ai/chatbot/aiinsurnacechatbot/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.insurance.ai.chatbot.aiinsurnacechatbot.config;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,5 +10,10 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    ChatClient chat(ChatClient.Builder builder) {
+        return builder.build();
     }
 }
